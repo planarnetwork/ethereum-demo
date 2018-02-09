42a11826f2994ad720c671daac85fc769a08bc08
Linus Norton <linusnorton@gmail.com>
2018-02-09 04:15:49 +0000

    removed typechain

--- contracts/Flow.sol
+++ contracts/Flow.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.19;
 
 /**
  * The flow contract returns fares between a specific origin and destination
--- migrations/3_deploy_flow.js
+++
@@ -1,5 +0,0 @@
-const Flow = artifacts.require("Flow");
-
-module.exports = function deploy(deployer) {
-  deployer.deploy(Flow);
-}
